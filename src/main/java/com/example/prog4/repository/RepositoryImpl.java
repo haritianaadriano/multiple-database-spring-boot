09f5410995ef72407e69aa3b1e64dabf73035a88
RyanIaro <hei.ryan.3@gmail.com>
2023-08-21 10:19:48 +0300

    refactor: renamed repository implementation

--- src/main/java/com/example/prog4/repository/RepositoryImpl.java
+++ src/main/java/com/example/prog4/repository/RepositoryImpl.java
@@ -1,26 +1,22 @@
 package com.example.prog4.repository;
 
-import com.example.prog4.model.EmployeeFilter;
 import com.example.prog4.model.exception.NotFoundException;
 import com.example.prog4.repository.postgres1.EmployeeRepository;
-import com.example.prog4.repository.postgres1.dao.EmployeeManagerDao;
 import com.example.prog4.repository.postgres1.entity.Employee;
 import com.example.prog4.repository.postgres2.CNAPSRepository;
 import com.example.prog4.repository.postgres2.entity.CNAPSEmployee;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 @AllArgsConstructor
-public class RepostiroyImpl implements com.example.prog4.repository.Repository {
+public class RepositoryImpl implements com.example.prog4.repository.Repository {
     private EmployeeRepository employeeRepository;
     private CNAPSRepository cnapsRepository;
 
     @Override
     public Employee findById(String idEmployee) {
-        return employeeRepository.findById(idEmployee).orElseThrow(() -> new NotFoundException("Employee not found"));
+        return employeeRepository.findById(idEmployee).orElseThrow(() -> new NotFoundException("Not found id=" + idEmployee));
     }
 
     @Override
